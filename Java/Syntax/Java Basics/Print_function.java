b9d5799679e3dd4fcee1f19d6a9b1e43c7019a54
Akshat-1407 <akshat.rastogi1407@gmail.com>
2026-06-25 22:03:12 +0530

    ...

--- Java/Syntax/Java Basics/Print_function.java
+++
@@ -1,7 +0,0 @@
-public class Print_function {
-    public static void main(String[] args) {
-        
-        System.out.println("println() will add a new line");
-        System.out.print("print() will not add a new line");
-    }
-}
